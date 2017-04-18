3fbdacb1cb70c8d367953ea5630987db7a5b15b9
Jared Still <still@pythian.com>
2017-04-18 19:11:30 -0400

    collect index plan items from v$sql_plan

--- vsql-idx.pl
+++ vsql-idx.pl
@@ -10,65 +10,80 @@ use Getopt::Long;
 
 my %optctl = ();
 
-Getopt::Long::GetOptions(
-	\%optctl,
-	"database=s",
-	"username=s",
-	"password=s",
-	"sysdba!",
-	"sysoper!",
-	"z","h","help");
-
-my($db, $username, $password, $connectionMode);
-
-$connectionMode = 0;
-if ( $optctl{sysoper} ) { $connectionMode = 4 }
-if ( $optctl{sysdba} ) { $connectionMode = 2 }
-
-if ( ! defined($optctl{database}) ) {
-	usage(1);
-}
-$db=$optctl{database};
-
-if ( ! defined($optctl{username}) ) {
-	usage(2);
-}
-
-$username=$optctl{username};
-$password = $optctl{password};
-
-#print "USERNAME: $username\n";
-#print "DATABASE: $db\n";
-#print "PASSWORD: $password\n";
-#exit;
-
 my $dbh = DBI->connect(
-	'dbi:Oracle:' . $db,
-	$username, $password,
+	'dbi:Oracle:' ,
+	undef, undef,
 	{
 		RaiseError => 1,
 		AutoCommit => 0,
-		ora_session_mode => $connectionMode
+		ora_session_mode => 2
 	}
 	);
 
-die "Connect to  $db failed \n" unless $dbh;
+die "Connect to  oracle failed \n" unless $dbh;
+
+my  $lastTimeStampFile='./last-timestamp.txt';
+my  $lastTimeStamp = '2017-01-01 00:00:00';
+
+if ( -r $lastTimeStampFile ) {
+	open TS,'<',$lastTimeStampFile || die " cannot open $lastTimeStampFile - $!\n";
+	my @timestamps=<TS>;
+	close TS;
+	$lastTimeStamp = $timestamps[$#timestamps];
+		
+}
+
+my $outputFile = 'vsql-idx.csv';
+if ( ! -r $outputFile ) {
+	open OF,'>',$outputFile || die " cannot open $outputFile - $!\n";
+	print OF join(',',qw[timestamp sql_id plan_hash_value inst_id object_name objectnum]), "\n";
+	close OF;
+}
+
+print "Timestamp: $lastTimeStamp\n";
 
 # apparently not a database handle attribute
 # but IS a prepare handle attribute
 #$dbh->{ora_check_sql} = 0;
 $dbh->{RowCacheSize} = 100;
 
-my $sql=q{select user from dual};
+my $sql=q{ select
+	to_char(timestamp,'yyyy-mm-dd hh24:mi:ss') timestamp
+	, sql_id
+	, plan_hash_value
+	, inst_id
+	--, object_owner
+	, object_name
+	, object#
+	-- no partitions in CT schema
+	--, partition_start
+	--, partition_stop
+	--, partition_id
+from gv$sql_plan
+where object_owner = 'CT'
+	and object_type = 'INDEX'
+	and timestamp > to_date(?,'yyyy-mm-dd hh24:mi:ss')
+order by 1,2,3};
+
+open OF,'>>',$outputFile || die " cannot open $outputFile - $!\n";
 
 my $sth = $dbh->prepare($sql,{ora_check_sql => 0});
 
-$sth->execute;
+$sth->execute($lastTimeStamp);
 
+my $rowCount=0;
 while( my $ary = $sth->fetchrow_arrayref ) {
-	#print "\t\t$${ary[0]}\n";
-	print "\t\t$ary->[0]\n";
+	$rowCount++;
+	my ($timeStamp,$sqlID,$planHashValue,$instanceID,$objectName,$objectNum) = @{$ary};
+	print OF join(',',@{$ary}),"\n";
+	$lastTimeStamp = $timeStamp;
 }
+
+open TS,'>',$lastTimeStampFile || die " cannot open $lastTimeStampFile for write - $!\n";
+
+print "Rows added: $rowCount\n";
+
+print TS "$lastTimeStamp";
 
 $dbh->disconnect;
 
