69cffaf60fe9b66917d1fe25c93847961b79790d
Jared Still <jkstill@gmail.com>
2017-04-27 14:51:27 -0700

    use index_type to filter indexes

--- unused-ct-indexes.sql
+++ unused-ct-indexes.sql
@@ -33,8 +33,7 @@ left outer join cons_idx idx on idx.table_name = i.table_name
 	and idx.index_name = i.index_name
 where i.owner = 'CT'
 and i.index_name not in (select * from avail.used_ct_indexes)
-and i.index_name not like 'SYS_%$$' -- LOB segments
-and i.index_name not like 'DR$%$%' -- Text indexes
+and i.index_type in ('NORMAL','NORMAL/REV','FUNCTION-BASED NORMAL')
 -- not going to rely on index naming convention
 --and index_name not like '%\_UK' escape '\'  -- Unique Keys - a fortunate naming convention
 --and index_name not like '%\_PK' escape '\'  -- Primary Keys - a fortunate naming convention
