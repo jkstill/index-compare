2e6212725ee5d8dfc70ff29996b447b5ff1dcf5b
Jared Still <jkstill@gmail.com>
2017-07-21 09:05:12 -0700

    made several modifications to deal with comparing many schemas rather than just one

--- unused-ct-indexes.sql
+++ unused-ct-indexes.sql
@@ -1,8 +1,8 @@
 
-
 @@get-schema &1
 
 set term on echo off term on feed on
+set verify off
 
 set pagesize 100
 
@@ -17,39 +17,50 @@ def b1='#######################################'
 def b2='### '
 
 prompt &b1
-prompt &b2 CT Indexes that MAY not be used
+prompt &b2 Indexes that MAY not be used
 prompt &b1
 
 accept dummy prompt 'Press <ENTER> to continue '
 
+spool unused-ct-indexes.txt
+
 break on report
 compute sum of bytes on report
 
-with cons_idx as (
+with owners as (
+	select username
+	from dba_users
+	where default_tablespace not in ('SYSTEM','SYSAUX')
+),
+cons_idx as (
 	select /*+ no_merge */
-		table_name, index_name , constraint_type
+		owner,table_name, index_name , constraint_type
 	from dba_constraints
-	where owner = '&u_schema'
-	and constraint_type in ('R','U','P')
+	where constraint_type in ('R','U','P')
 	and index_name is not null
+	and owner in ( 
+		select username from owners
+	)
 )
-select i.table_name, i.index_name
+select i.owner, i.table_name, i.index_name
 	, i.leaf_blocks * t.block_size bytes
 	, idx.constraint_type -- sanity check - should be all nulls
 from dba_indexes i
 join dba_tablespaces t on i.tablespace_name = t.tablespace_name
-left outer join cons_idx idx on idx.table_name = i.table_name
+left outer join cons_idx idx on idx.owner = i.owner
+	and idx.table_name = i.table_name
 	and idx.index_name = i.index_name
-where i.owner = '&u_schema'
-and i.index_name not in (select * from &u_schema..used_ct_indexes)
+where (i.owner,i.index_name) not in (select owner,index_name from &u_schema..used_ct_indexes)
 and i.index_type in ('NORMAL','NORMAL/REV','FUNCTION-BASED NORMAL')
--- not going to rely on index naming convention
---and index_name not like '%\_UK' escape '\'  -- Unique Keys - a fortunate naming convention
---and index_name not like '%\_PK' escape '\'  -- Primary Keys - a fortunate naming convention
-and (i.table_name, i.index_name) not in (
-	select table_name, index_name from cons_idx
+and (i.owner, i.table_name, i.index_name) not in (
+	select owner, table_name, index_name from cons_idx
 )
-order by 3
+and i.owner in ( select username from owners)
+order by 4 desc
 /
+
+spool off
+
+--ed unused-ct-indexes.txt
 
 
