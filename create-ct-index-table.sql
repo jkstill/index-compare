754b0da037d6bc292e934764a8e3b0415e59e1e4
Jared Still <jkstill@gmail.com>
2017-08-16 12:26:26 -0700

    several tables added to track sql and plans; workaround to get sql text

--- create-ct-index-table.sql
+++ create-ct-index-table.sql
@@ -2,7 +2,52 @@
 
 @@get-schema &1
 
-create table &&u_schema..used_ct_indexes ( owner varchar2(30) not null, index_name varchar2(30) not null, primary key (owner,index_name) ) organization index
+create table &&u_schema..used_ct_indexes (
+	owner varchar2(30) not null,
+	index_name varchar2(30) not null,
+	primary key (owner,index_name)
+) organization index
+/
+
+create table &&u_schema..used_ct_index_plans (
+	plan_hash_value number not null,
+	plan_text clob,
+	primary key (plan_hash_value)
+) organization index
+/
+
+create table &&u_schema..used_ct_index_sql (
+	sql_id varchar2(13) not null,
+	sql_text clob,
+	primary key (sql_id)
+) organization index
+/
+
+create table &&u_schema..used_ct_index_sql_plan_pairs (
+	owner varchar2(30) not null,
+	index_name varchar2(30) not null,
+	plan_hash_value number not null,
+	sql_id varchar2(13) not null,
+	primary key (owner,index_name,plan_hash_value,sql_id)
+)
+/
+
+create index &&u_schema..used_ct_index_sqlid_idx on &&u_schema..used_ct_index_sql_plan_pairs(sql_id);
+create index &&u_schema..used_ct_index_plans_idx on &&u_schema..used_ct_index_sql_plan_pairs(plan_hash_value);
+
+alter table &&u_schema..used_ct_index_sql_plan_pairs add constraint sql_plan_pairs_parent_fk
+foreign key (owner,index_name) 
+references &&u_schema..used_ct_indexes (owner,index_name)
+/
+
+alter table &&u_schema..used_ct_index_sql_plan_pairs add constraint used_ct_index_sql_fk 
+foreign key(sql_id)
+references  &&u_schema..used_ct_index_sql(sql_id)
+/
+
+alter table &&u_schema..used_ct_index_sql_plan_pairs add constraint used_ct_index_plans_fk
+foreign key(plan_hash_value)
+references &&u_schema..used_ct_index_plans
 /
 
 set echo off 
---
+++ get-sqltext.sql
@@ -0,0 +1,18 @@
+
+-- How to get full sql text statement from v$sql (Doc ID 437304.1)
+-- 
+-- this is a workaround to getting sql from v$sqltext, as sql_fulltext column is erroneously trimmed (bug) in many 11g versions
+--
+
+set linesize 132 pagesize 999
+column sql_fulltext format a60 word_wrap 
+break on sql_text skip 1 
+
+select 
+	replace(translate(sql_fulltext,'0123456789','999999999'),'9','') SQL_FULLTEXT 
+from v$sql 
+where sql_id = '8jxxt9p48w0bs'
+group by replace(translate(sql_fulltext,'0123456789','999999999'),'9','') 
+/
+
+
